35112b00c0f4cc7628b1360d4c8ab003dc887c2f
memento999 <janokakashvili@gmail.com>
2020-07-11 13:58:33 +0400

    my first commit

---
+++ scripts/swipers/main-swiper.js
@@ -0,0 +1,13 @@
+// Initialize Swiper
+
+var swiper = new Swiper(".main__swiper", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
